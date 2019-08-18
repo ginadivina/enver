a945f8cca8b0b2c6b60666241e85077c5fb1774c
harry <harry.soiland@rocketboots.com>
2019-08-18 20:11:44 +1000

    almost get the data into bsv just need to set user id dynamically

--- client/src/components/forms/question/QuestionForm.js
+++ client/src/components/forms/question/QuestionForm.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button, Input, Form } from "antd";
 import { connect } from "react-redux";
+import MoneyButton from '@moneybutton/react-money-button'
 
 const {TextArea} = Input;
 
 const QuestionForm = (props) => {
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(event.target);
-    const data = new FormData(event.target);
-  }
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  // const [code, setCode] = useState('');
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     console.log(body)
+    // }
 
   return (
     <>
       <h1>New Question</h1>
-      <Form onSubmit={handleSubmit}>
-        <Input name="title" placeholder="What is it you need help with?" />
-        <TextArea name="body" rows={4} placeholder="Describe Your issue."/>
-        <TextArea name="code" rows={4} placeholder="Paste your code into here."/>
-        <Button name="submit" htmlType="submit" value="Submit"/>
-      </Form>
+      {/*<Form onSubmit={handleSubmit}>*/}
+        <Input name="title"  placeholder="What is it you need help with?" onChange={event => setTitle(event.target.value)}/>
+        <TextArea name="body" rows={4} placeholder="Describe Your issue." onChange={event => setBody(event.target.value)}/>
+        {/*<TextArea name="code" rows={4} placeholder="Describe Your issue." onChange={event => setCode(event.target.value)}/>*/}
+        {/*<Button name="submit" htmlType="submit" value="Submit"/>*/}
+      {/*</Form>*/}
+      <div>
+        <p>Bounty your question</p>
+        <MoneyButton
+            to={"9552"}
+            amount={"0.01"}
+            currency={'USD'}
+        />
+      </div>
+
     </>
   )
 };
