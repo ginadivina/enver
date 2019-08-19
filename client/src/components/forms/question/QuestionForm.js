fa89811e0e27f392f5fb2f117b8bf04d5c156ed9
harry <harrysoiland@gmail.com>
2019-08-19 20:20:25 +1000

    route after payment

--- client/src/components/forms/question/QuestionForm.js
+++ client/src/components/forms/question/QuestionForm.js
@@ -44,7 +44,7 @@ export default class questionForm extends React.Component {
 
    async handleOnPayment () {
       await sleep(4000);
-      window.location = '/'
+      window.location = '/questions'
   }
 
   render () {
@@ -83,7 +83,7 @@ export default class questionForm extends React.Component {
                 outputs={[{
                   'userId':this.state.u,
                   'script': this.state.script,
-                  'amount': "0.00001",
+                  'amount': "0",
                   'currency': "BSV"
                 },
                   {
